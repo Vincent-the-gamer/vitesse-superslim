71520d36354729a928107e6fb6db2a5a25840f77
Vincent-the-gamer <erxianqiaottjt@163.com>
2024-09-23 17:09:32 +0800

    chore: ignore node_modules in eslint config.

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,10 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   typescript: true,
   vue: true,
-  ignores: ['**/fixtures'],
+  ignores: [
+    '**/fixtures',
+    'node_modules',
+  ],
   formatters: {
     /**
      * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
